095558eef615e45800902ddac3980ba595ba6499
Ernst Stolz <eric@spiritsdontfly.com>
2024-04-07 18:51:17 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+
+      <p className="read-the-docs">
+        Click on a square to place your mark. The first player to get three in a
+        row wins!
+      </p>
+    </>
+  );
+}
+
+export default App;
