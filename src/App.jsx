1e45ccf347811e7c9c57cc7466d7b7f1c54edc51
Ernst Stolz <eric@spiritsdontfly.com>
2024-04-07 18:57:03 +0200

    add initial state for the field, show empty buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,56 @@
+import { useState } from "react";
 import "./App.css";
 
+const initialState = [
+  {
+    id: 0,
+    player: undefined,
+  },
+  {
+    id: 1,
+    player: undefined,
+  },
+  {
+    id: 2,
+    player: undefined,
+  },
+  {
+    id: 3,
+    player: undefined,
+  },
+  {
+    id: 4,
+    player: undefined,
+  },
+  {
+    id: 5,
+    player: undefined,
+  },
+  {
+    id: 6,
+    player: undefined,
+  },
+  {
+    id: 7,
+    player: undefined,
+  },
+  {
+    id: 8,
+    player: undefined,
+  },
+];
+
 function App() {
+  const [fieldState, setFieldState] = useState(initialState);
   return (
     <>
       <h1>Tic Tac Toe</h1>
+
+      <div className="card">
+        {fieldState.map((field) => (
+          <button key={field.id}>{field.player || ""}</button>
+        ))}
+      </div>
 
       <p className="read-the-docs">
         Click on a square to place your mark. The first player to get three in a
